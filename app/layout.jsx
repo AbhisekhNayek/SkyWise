5cba19d48d011f1baec0efaaad25c174a113f873
AbhisekhNayek <115914826+AbhisekhNayek@users.noreply.github.com>
2024-10-18 19:40:26 +0530

    fixed bugs

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,12 +7,17 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "SkyWise",
   description: "A weather app built with Next.js and TailwindCSS",
+  keywords: "weather, app, next.js, tailwindcss",
+  author: "Your Name",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <head>
+        {/* You can add additional head elements here */}
+      </head>
+      <body className={inter.className} suppressHydrationWarning={true}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
